118daa23118b00ce3f5b0cb5ba322040508f1d53
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-13 00:05:58 -0800

    AI gemini setup

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -11,6 +11,13 @@ import {
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
+import { chatSession } from '@/utils/GeminiAiModal';
+import { LoaderCircle } from 'lucide-react';
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { v4 as uuidv4 } from 'uuid'
+import { useUser } from '@clerk/nextjs';
+import moment from 'moment/moment';
 
 
 const AddNewInterview = () => {
@@ -18,6 +25,42 @@ const AddNewInterview = () => {
     const [jobPosition, setJobPosition] = useState();
     const [jobDesc, setJobDesc] = useState();
     const [jobExp, setJobExp] = useState();
+    const [loading, setLoading] = useState(false);
+    const [jsonResponse, setJsonResponse] = useState([])
+    const { user } = useUser();
+
+    const onSubmit = async (e) => {
+        setLoading(true)
+        e.preventDefault();
+        console.log(jobDesc);
+
+        const InputPrompt = "Job Position: " + jobPosition + ", Job Description: " + jobDesc + ", Years of Expirence: " + jobExp + ". Depends on Job Position Description and Years of Expirence give us 5 questions alonf=g with Answer in JSON Format. Give us question and answer field on JSON"
+        const result = await chatSession.sendMessage(InputPrompt)
+        const mockJsonResp = (result.response.text()).replace('```json', '').replace('```', '')
+        console.log(mockJsonResp);
+        setJsonResponse(mockJsonResp)
+        if (mockJsonResp) {
+            const resp = await db.insert(mockInterview)
+                .values({
+                    mockId: uuidv4(),
+                    jsonMockResp: mockJsonResp,
+                    jobPosition: jobPosition,
+                    jobDesc: jobDesc,
+                    jobExperience: jobExp,
+                    createdBy: user?.primaryEmailAddress?.emailAddress,
+                    createdAt: moment().format('DD-MM-yyyy')
+                }).returning({ mockId: MockInterview.mockId })
+
+            console.log("Inserted Id: ", resp);
+        } else {
+            console.log("Error");
+            
+        }
+
+
+        setLoading(false)
+
+    }
     return (
         <div>
             <div onClick={() => setOpenDialog(true)} className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all'>
@@ -29,7 +72,7 @@ const AddNewInterview = () => {
                     <DialogHeader>
                         <DialogTitle>Tell us more about your job interviewing</DialogTitle>
                         <DialogDescription>
-                            <form action="">
+                            <form action="" onSubmit={onSubmit}>
                                 <div>
 
                                     <h2 className='text-2xl'>Add Details about your job position/role, Job description and years of expirence</h2>
@@ -50,7 +93,11 @@ const AddNewInterview = () => {
                                 </div>
                                 <div className='flex gap-5 justify-end'>
                                     <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
-                                    <Button type="submit">Start Interview</Button>
+                                    <Button type="submit" disabled={loading}>
+                                        {
+                                            loading ? <LoaderCircle className='animate-spin' /> : 'Start Interview'
+                                        }
+                                    </Button>
                                 </div>
                             </form>
                         </DialogDescription>
