e7a81b7fb4e98ebe8dbba65bf457404b06e33782
Yash Pokharna <yashpokharna2512@gmail.com>
2025-02-17 16:54:53 -0800

    start interview page

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { db } from '@/utils/db';
 import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
-import { WebcamIcon } from 'lucide-react';
+import { Lightbulb, WebcamIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import Webcam from 'react-webcam';
 
@@ -21,28 +21,44 @@ const Interview = ({ params }) => {
         setInterviewData(result[0]);
     }
     return (
-        <div className='my-10 flex justify-center flex-col items-center'>
+        <div className='my-10 '>
             <h2 className='font-bold text-2xl '>Let's get started</h2>
-            <div className=''>
-                {webCamEnable 
-                    ? <Webcam 
-                        onUserMedia={()=>setWebCamEnable(true)} 
-                        onUserMediaError={()=>setWebCamEnable(false)} 
-                        mirrored={true}
-                        style={{ height: 300, width: 300 }} /> 
-                    : 
-                    <>
-                        <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border' />
-                        <Button onClick={() => setWebCamEnable(true)}>Enable Web cam & Microphone</Button>
-                    </>
-                }
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
 
+                <div className='flex flex-col my-5 gap-5 p-5 rounded-lg border'>
+                    <div className='flex flex-col p-5 rounded-lg border gap-5'>
+                        <h2 className='text-lg'>Job Role/ Job Position: {interviewData.jobPosition}</h2>
+                        <h2 className='text-lg'>Job Description/ Tech Stack: {interviewData.jobDesc}</h2>
+                        <h2 className='text-lg'>Years of Experience {interviewData.jobExperience}</h2>
+                    </div>
+                    <div className='p-5 border rounded-lg border-yellow-300 bg-yellow-100'>
+                        <h2 className='flex items-center gap-2 text-yellow-500'><Lightbulb /><strong>Information</strong></h2>
+                        <p className='mt-3 text-yellow-500'>Enable Video Web cam and Microphone to start your AI Generated Mock Interview. It has 5 questions on the basis of your answer. NOTE: we never record your video, web cam access you can disable at any time if you want</p>
+
+                    </div>
+
+                </div>
+                <div className=''>
+                    {webCamEnable
+                        ? <Webcam
+                            onUserMedia={() => setWebCamEnable(true)}
+                            onUserMediaError={() => setWebCamEnable(false)}
+                            mirrored={true}
+                            style={{ height: 300, width: 300 }} />
+                        :
+                        <>
+                            <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border' />
+                            <Button variant="ghost" onClick={() => setWebCamEnable(true)}>Enable Web cam & Microphone</Button>
+                        </>
+                    }
+
+                </div>
             </div>
-            <div className='flex flex-col my-5'>
-                <h2 className='text-lg'>Job Role/ Job Position: {interviewData.jobPosition}</h2>
-                <h2 className='text-lg'>Job Description/ Tech Stack: {interviewData.jobDesc}</h2>
-                <h2 className='text-lg'>Years of Experience {interviewData.jobExperience}</h2>
+            <div className='flex justify-end items-end'>
+                <Button >Start Interview</Button>
             </div>
+
+
         </div>
     )
 }
